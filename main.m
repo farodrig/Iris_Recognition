1c96624c5c69a013fb65b3815a7e9089e2a3be0c
farodrig <farodrig@dcc.uchile.cl>
2014-11-06 20:43:55 -0300

    Arreglos en segmentación y main
    Hay aprox un 15% de error al segmentar el iris.

--- main.m
+++ main.m
@@ -1,4 +1,6 @@
-imgs = LoadFiles('DataSet');
+if isempty(imgs)
+    imgs = LoadFiles('DataSet');
+end
 centers = zeros(size(imgs,2), 4);
 radios = zeros(size(imgs,2), 2);
 for i = 1:size(imgs,2)
@@ -6,5 +8,5 @@ for i = 1:size(imgs,2)
     centers(i,:) = [center(1,:) center(2,:)]; %Primer par es de la pupila, segundo par es el del iris.
     radios(i,:) = [radio(1) radio(2)];
 end
-save(centers,'centers');
-save(radios,'radios');
+save('centers.mat','centers');
+save('radios.mat','radios');
--- segmentation.m
+++ segmentation.m
@@ -3,43 +3,67 @@ function [ centers, radius ] = segmentation(iris)
 %   Detailed explanation goes here
     img = rgb2gray(iris);    
     BW = edge(img,'canny');
-    [centersPupil, radiiPupil, ~] = imfindcircles(BW,[20 40], 'method', 'TwoStage', 'EdgeThreshold',0.2);
-    [centersIris, radiiIris, ~] = imfindcircles(BW,[43 73], 'method', 'TwoStage', 'EdgeThreshold',0.2);
-    center = [size(img,1)/2, size(img,2)/2];
-    if(numel(centersIris)>2)
-        min = 5000;
-        for i = 1:size(centersIris,1)
-            dist = distanceEuc(centersIris(i,1), centersIris(i,2), center(1), center(2));
-            if dist<min
-                candcen = centersIris(i,:);
-                candrad = radiiIris(i);
+    irisLimits = [20 37 35 73; 20 37 30 73; 20 39 40 73];
+    centersPupil = [];
+    centersIris = [];
+    l = 1;
+    while (isempty(centersPupil) || isempty(centersIris))
+        if (l>size(irisLimits,1))
+            centersPupil = [0 0];
+            centersIris = [0 0];
+            radiiPupil = 0;
+            radiiIris = 0;
+            break;
+        end
+        [centersPupil, radiiPupil, ~] = imfindcircles(BW,[irisLimits(l,1) irisLimits(l,2)], 'method', 'TwoStage', 'EdgeThreshold',0.2);
+        [centersIris, radiiIris, ~] = imfindcircles(BW,[irisLimits(l,3) irisLimits(l,4)], 'method', 'TwoStage', 'EdgeThreshold',0.2);
+        if (isempty(centersPupil) || isempty(centersIris))
+            l = l+1;
+            continue;
+        end
+        center = [size(img,1)/2, size(img,2)/2];
+        if(numel(centersIris)>2)
+            min = 5000; cont = 0;
+            for i = 1:size(centersIris,1)
+                dist = distanceEuc(centersIris(i,1), centersIris(i,2), center(1), center(2));
+                if dist<min && countInsideCircle(centersIris(i,:), radiiIris(i), centersPupil, radiiPupil)>0
+                    cont = cont+1;
+                    min = dist;
+                    candcen = centersIris(i,:);
+                    candrad = radiiIris(i);
+                end
+            end
+            if cont~=0
+                centersIris = candcen;
+                radiiIris = candrad;
+            end
+        end    
+        while(numel(centersPupil)>2)
+            index = [];
+            for i = 1:size(centersPupil,1)
+                if ~(insideCircle(centersIris(1,:), radiiIris(1), centersPupil(i,:), radiiPupil(i)))
+                    index = [index i];
+                end
+            end
+            [centersPupil, ~] = removerows(centersPupil,'ind', index);
+            [radiiPupil, ~] = removerows(radiiPupil,'ind', index);
+            min = 5000; cont = 0;
+            for i = 1:size(centersPupil,1)
+                dist = distanceEuc(centersPupil(i,1), centersPupil(i,2), center(1), center(2));
+                if dist<min
+                    cont = cont+1;
+                    min = dist;
+                    candcen = centersPupil(i,:);
+                    candrad = radiiPupil(i);
+                end
+            end
+            if cont~=0
+                centersPupil = candcen;
+                radiiPupil = candrad;
             end
         end
-        centersIris = candcen;
-        radiiIris = candrad;
-    end    
-    while(numel(centersPupil)>2)
-        index = [];
-        for i = 1:size(centersPupil,1)
-            if ~(insideCircle(centersIris(1,:), radiiIris(1), centersPupil(i,:), radiiPupil(i)))
-                index = [index i];
-            end
-        end
-        [centersPupil, ~] = removerows(centersPupil,'ind', index);
-        [radiiPupil, ~] = removerows(radiiPupil,'ind', index);
-        min = 5000;
-        for i = 1:size(centersPupil,1)
-            dist = distanceEuc(centersPupil(i,1), centersPupil(i,2), center(1), center(2));
-            if dist<min
-                min = dist;
-                candcen = centersPupil(i,:);
-                candrad = radiiPupil(i);
-            end
-        end
-        centersPupil = candcen;
-        radiiPupil = candrad;
+        l = l+1;
     end
-    
     centers = [centersPupil; centersIris];
     radius = [radiiPupil; radiiIris];
     
@@ -50,6 +74,15 @@ function [ centers, radius ] = segmentation(iris)
     function bool = insideCircle(center1, rad1, center2, rad2)
         distance = distanceEuc(center1(1), center1(2), center2(1), center2(2));
         bool = distance>0 && distance<abs(rad1 - rad2);
+    end
+
+    function count = countInsideCircle(center, rad, centers, radios)
+        count = 0;
+        for n = 1:size(centers, 1)
+            if insideCircle(center, rad, centers(n,:), radios(n))
+                count = count + 1;
+            end
+        end
     end
 end
 
