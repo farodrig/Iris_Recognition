20be7deb0dfda226a512fee93923d65a675b6bab
luism <luis.martinezqug.uchile.cl>
2014-11-16 17:14:36 -0300

    Agregada funcion que genera el iris-code, no se si funciona bien

--- createCode.m
+++ createCode.m
@@ -1,5 +1,22 @@
 function iriscode=createCode(img)
 %Create the iris code for matching, img must be a rectangular iris image
-    gaborArray=gaborFilterBank(5,8,39,39);
-    [gaborResult vector]=gaborFeatures(img,gaborArray,15,10);
-ends 
+%usamos solo un filtro de gabor.
+    nscales=1; %numero de filtros
+    gaborArray=gaborFilterBank(1,1,39,39);
+    [gaborResult vector]=gaborFeatures(img,gaborArray,5,5);
+    E1 = gaborResult{1,1};
+    k=1;
+    length = size(img,2)*2*nscales;
+    iriscode = zeros(size(img,1), length);
+    length2 = size(img,2);
+    h = 1:size(img,1);
+    %Phase quantisation
+    H1 = real(E1) > 0;
+    H2 = imag(E1) > 0;
+    for i=0:(length2-1)             
+        ja = double(2*nscales*(i));
+        %construct the biometric template
+        iriscode(h,ja+(2*k)-1) = H1(h, i+1);
+        iriscode(h,ja+(2*k)) = H2(h,i+1);
+    end
+end 
