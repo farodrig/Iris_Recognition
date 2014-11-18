f0dc8a9ba1da47a8aa685270c1d6f27ad2cb680b
farodrig <farodrig@dcc.uchile.cl>
2014-11-17 22:17:37 -0300

    Subido script para la presentación

--- makeRectangle.m
+++ makeRectangle.m
@@ -3,11 +3,12 @@ function [ rectangle ] = makeRectangle(iris, centers, radios, length, width, typ
 %   Type==1: sin filtro gaussiano
 %   Type==2: con filtro gaussiano
     rectangle = zeros(width, length, 'uint8');
-    img = isolateIris(iris, centers, radios);
+    img = isolateIris(rgb2gray(iris), centers, radios);
     center = [(centers(2)+centers(4))/2, (centers(1)+centers(3))/2];
     j=1;
     for theta = degtorad(linspace(0, 360, length))
         i = 1;
+        imshow(img);
         [minrad, maxrad] = rangeIris(img, center, theta);
         for r = linspace(minrad+1, maxrad-1, width)
             x = floor(center(1) + r*cos(theta));
@@ -68,7 +69,7 @@ function [minr, maxr] = rangeIris(img, center, theta)
     while maxr == 0
         xNext = floor(center(1) + r*cos(theta));
         yNext = floor(center(2) + r*sin(theta));
-        data = img(xNext, yNext);
+        data = img(xNext, yNext);        
         if (minr == 0 && data~=255)
             minr = r;
         end
